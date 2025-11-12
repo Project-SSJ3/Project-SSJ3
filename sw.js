d1fb6025bdf361920602c35554e864a31e6efead
Project-SSJ3 <fatouilian6@gmail.com>
2025-11-12 17:16:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ssj3botz-pwa-v1';
+const CACHE_NAME = 'ssj3botz-v2';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
